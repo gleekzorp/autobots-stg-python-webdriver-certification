61a7b1e64828775ed9965828b67972643a23c88b
gleekzorp <gleek.dan@gmail.com>
2019-08-29 18:33:17 -0600

    Updated some variables

--- challenges/challenge2/challenge2.py
+++ challenges/challenge2/challenge2.py
@@ -16,10 +16,15 @@ class Challenge2(unittest.TestCase):
         self.driver.close()
 
     def test_challenge2(self):
+        search_term = "exotics"
+        search_word = 'ROLLS-ROYCE'
         self.driver.get('https://www.copart.com/')
-        input_field = self.driver.find_element_by_id('input-search')
-        input_field.send_keys('exotics')
-        input_field.send_keys(Keys.ENTER)
+        # input_field = self.driver.find_element_by_id('input-search')
+        # input_field.send_keys(search_term)
+        # input_field.send_keys(Keys.ENTER)
+
+        # How to chain
+        self.driver.find_element_by_id('input-search').send_keys(search_term + Keys.ENTER)
 
         results_table = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "serverSideDataTable"))
@@ -29,7 +34,7 @@ class Challenge2(unittest.TestCase):
         # results_table = self.driver.find_element_by_id('serverSideDataTable')
 
         results_table_source = results_table.get_attribute('innerHTML')
-        self.assertIn('PORSCHE', results_table_source)
+        self.assertIn(search_word, results_table_source)
 
         # print(results_table_source)
 
